44f6371cc936d9ba762f79df1ad6bf409fa0f7c2
mmarakesh <marinamakarova749@gmail.com>
2024-01-19 20:11:26 +0500

    add button toggle lang

--- src/App.js
+++ src/App.js
@@ -8,14 +8,31 @@ Link
 import Makeup from "./Makeup";
 import Home from "./Home";
 import Promotion from "./Promotion";
+import { useTranslation } from 'react-i18next';
 import './App.css';
 
 function App() {
+
+  const { i18n, t } = useTranslation();
+
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en')
+  }
+  
   return <Router>
       <nav>
-        <Link to="/" className="link">Home</Link>
-        <Link to="/makeup" className="link">Makeup</Link>
-        <Link to="/promotion" className="link">Promotion</Link>
+        <Link to="/" className="link">{t('Home')}</Link>
+        <Link to="/makeup" className="link">{t('Makeup')}</Link>
+        <Link to="/promotion" className="link">{t('Promotion')}</Link>
+        
+        <div className="btn-container">
+        <button className="btn-home" onClick={toggleLang}>
+        {i18n.language === 'en' ? 'ru' : 'en'}
+        </button>
+        </div>
+
+        
+
       </nav>
 
       <Routes>
