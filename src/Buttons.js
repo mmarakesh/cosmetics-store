895a1b170f700c49ef724e4656a105e97a005630
mmarakesh <marinamakarova749@gmail.com>
2024-01-24 16:32:00 +0500

    changes to Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -27,8 +27,8 @@ function Buttons({filteredCosmetics, setCosmetics, data}) {
     else if (cosmeticsName.name.toLowerCase().includes(search.toLocaleLowerCase())) {
     filteredItems.push(cosmeticsName);
      }
-
-     setCosmetics(filteredItems);
+    
+    else setCosmetics(filteredItems);
     })
     }
                 
@@ -43,14 +43,14 @@ function Buttons({filteredCosmetics, setCosmetics, data}) {
             <button className="change" onClick={() => filteredCosmetics("lip")}>{t('Lip')}</button>
             <button className="change" onClick={() => filteredCosmetics("cheek")}>{t('Cheek')}</button>
         </div>
-        <div>
+        <form>
          <input type="text" value={text} placeholder={t("search: lip, eye...")} onChange={handleText}></input>
          <button type="button" className="search" onClick={handleSearch}>
          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
         </svg>
          </button>
-         </div>
+         </form>
          </div>
     )
 }
